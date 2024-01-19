ec57f8b0bffbb24cd99a52bae12526856f311480
Gabriel de Mattos <mattos_95@hotmail.com>
2024-01-19 13:26:54 -0300

    chore: added comments and updated the constant name in the useUserLevel hook

--- src/games/motograu/hooks/useUserLevel.ts
+++ src/games/motograu/hooks/useUserLevel.ts
@@ -6,12 +6,20 @@ export const useUserLevel = (bets: []) => {
 
   const totalBets = bets.length
   const XP_PER_BET = 2
+  const TOTAL_BETS_REQUIRED_TO_UP_A_LEVEL = 50
 
   const calculateLevelAndXp = () => {
-    const xpForNextLevel = XP_PER_BET * 50
+    // Calcula a quantidade total de XP necessária para subir de nível (valor: 100)
+    const xpForNextLevel =
+      XP_PER_BET * TOTAL_BETS_REQUIRED_TO_UP_A_LEVEL
+
+    // Calcula o total de XP com base nas apostas feitas
     const totalXp = totalBets * XP_PER_BET
 
+    // Calcula o novo nível do usuário
     const newLevel = Math.floor(totalXp / xpForNextLevel) + 1
+
+    // Calcula a quantidade de XP restante para o próximo nível
     const remainingXp = totalXp % xpForNextLevel
 
     setUserLevel(newLevel)
